f9d591be1a4c4f49f5629cef4ad6c7b1d71908ea
Daboville Julien <julientrifiro@gmail.com>
2024-02-14 10:57:10 +0000

    supression de l'ancien Assets

--- Pause_Cafe/Assets/Scripts/CharacterGO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Characters;
-
-public class CharacterGO : MonoBehaviour
-{
-    public Character character;
-}
--- Pause_Cafe/Assets/Scripts/HexaGO.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Hexas;
-
-public class HexaGO : MonoBehaviour {
-	public Hexa hexa;
-}
